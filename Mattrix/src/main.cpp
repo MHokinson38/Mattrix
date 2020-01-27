5f6a5371acd99063b110bd20fcc6e26d3fa98b81
MHokinson38 <matthew@hokinson.com>
2020-01-27 09:39:55 -0600

    Cleaned Main Function of unused code

--- Mattrix/src/main.cpp
+++ Mattrix/src/main.cpp
@@ -22,33 +22,4 @@ int main( ){
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-//    CalculationUtil::InputLine inLine;
-//    std::cout << "Input calculation" << std::endl;
-//
-//    while(1) {
-//        std::string inputLine = "";
-//        std::getline(std::cin, inputLine);
-//
-//        if(inputLine.compare(".") == 0) {break;}
-//
-//        try {
-//            std::istrstream(inputLine.c_str()) >> inLine;
-//            std::cout << inLine << std::endl;
-//        }
-//        catch(MatrixUtil::InvalidSyntaxException& synError) {
-//            std::cerr << synError.what() << std::endl;
-//        }
-//        catch(MatrixUtil::UnsupportedComputationException& compuError) {
-//            std::cerr << compuError.what() << std::endl;
-//        }
-//        catch(MatrixUtil::ComputationalLogicException& logError) {
-//            std::cerr << logError.what() << std::endl;
-//        }
-//        catch(RandomUtils::InvalidCharacterException& charError) {
-//            std::cerr << charError.what() << std::endl;
-//        }
-//    }
-//    std::cout << "Goodbye" << std::endl;
-//
-//    return 0;
 }
